6d6376a58207a8932b5a0a024ae4b27d7ac1c2ad
dsandoval.ai <dsandoval.ai@gmail.com>
2025-07-12 21:05:48 +0000

    Adding Blog RRS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://astro-tutorial-1-dssh.netlify.app",
   integrations: [react()]
 });
